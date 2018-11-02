ddf0dc4ce1f756a1ae7a8cea6fb105431909db87
jdulal <jdulal@yahoo.com>
2018-11-02 15:45:08 +0545

    Second Update
    Second part of tutorial update.

--- app/src/main/java/jdulal/com/np/androidshopapp/MainActivity.java
+++ app/src/main/java/jdulal/com/np/androidshopapp/MainActivity.java
@@ -1,13 +1,27 @@
 package jdulal.com.np.androidshopapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin, btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.btnUserLogin);
+        btnSignup=findViewById(R.id.btnUserSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
